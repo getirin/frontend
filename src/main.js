710ae720a32dd08f16de0ff62f2de1ec00d244bb
cagataycali <cagataycali@icloud.com>
2018-02-17 01:15:34 +0300

    ✔️  Add google maps

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import Vue from 'vue'
+import * as VueGoogleMaps from 'vue2-google-maps'
 import Framework7 from 'framework7/dist/framework7.esm.bundle.js'
 import Framework7Vue from 'framework7-vue/dist/framework7-vue.esm.bundle.js'
 import Framework7Styles from 'framework7/dist/css/framework7.css'
@@ -11,8 +12,18 @@ import Routes from './routes.js'
 // Import App Component
 import App from './app'
 
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: 'AIzaSyA8VI7O2qYgTBk6Fymhm-pUm0Qq42IZOSA',
+    libraries: 'places,drawing,visualization'
+  }
+})
+
 // Init F7 Vue Plugin
 Vue.use(Framework7Vue, Framework7)
+
+Vue.component('google-map', VueGoogleMaps.Map)
+Vue.component('google-marker', VueGoogleMaps.Marker)
 
 new Vue({
   el: '#app',
--- src/pages/home.vue
+++ src/pages/home.vue
@@ -6,11 +6,50 @@
         <f7-link icon-if-ios="f7:menu" icon-if-md="material:menu" panel-open="right"></f7-link>
       </f7-nav-right>
     </f7-navbar>
-    <f7-block strong>
-      <p>H3ll0 W0RLD</p>
-    </f7-block>
+    <google-map
+      :center="center"
+      :zoom="7"
+      :options="options"
+      style="height: 100%"
+      >
+      <google-marker v-for="m in markers" :key="m.id" :position="m.position" :clickable="true" :draggable="true" @click="center=m.position"></google-marker>
+    </google-map>
   </f7-page>
 </template>
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      center: {
+        lat: 10.0,
+        lng: 10.0
+      },
+      markers: [
+        {
+          id: 1,
+          position: {
+            lat: 10.0,
+            lng: 10.0
+          }
+        },
+        {
+          id: 2,
+          position: {
+            lat: 11.0,
+            lng: 11.0
+          }
+        }
+      ],
+      options: {
+          disableDefaultUI: true,
+          zoomControl: true,
+          zoomControlOptions: {
+              position: 12
+          },
+          streetViewControl: false,
+          scrollwheel: true
+        },
+    }
+  }
+}
 </script>
